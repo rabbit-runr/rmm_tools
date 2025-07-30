d9d09b968a002164e7b76519479699d7ba6199a7
rabbit-runr <rabbit.runr08@gmail.com>
2025-07-30 15:47:21 -0500

    Update fetch_rmm_tool.py

--- fetch_rmm_tool.py
+++ fetch_rmm_tool.py
@@ -1,9 +1,10 @@
 import requests
+import json
 
 url = 'https://lolrmm.io/api/rmm_tools.json'
-data = requests.get(url).json()
-paths = data.get('InstallationPaths', {}).keys()
-
-with open("installation_paths.txt", "w") as f:
-    for p in sorted(paths):
-        f.write(f"{p}\n")
+response = requests.get(url)
+data = response.json()
+installation_paths = data.get("InstallationPaths", [])
+with open("installation_paths.txt", "w") as file:
+    for path in installation_paths:
+        file.write(path + "\n")
