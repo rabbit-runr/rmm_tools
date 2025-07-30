980ce07439493461e7c692c2e6d1f7f9cdb129de
rabbit-runr <rabbit.runr08@gmail.com>
2025-07-30 16:30:41 -0500

    Update fetch_rmm_tool.py

--- fetch_rmm_tool.py
+++ fetch_rmm_tool.py
@@ -1,10 +1,25 @@
 import requests
-import json
 
-url = 'https://lolrmm.io/api/rmm_tools.json'
+url = "https://lolrmm.io/api/rmm_tools.json"
+
 response = requests.get(url)
+response.raise_for_status()
+
 data = response.json()
-installation_paths = data.get("InstallationPaths", [])
+
+installation_paths = []
+
+for item in data:
+    details = item.get("Details", {})
+    paths = details.get("InstallationPaths")
+    
+    # Make sure paths is a list before extending
+    if isinstance(paths, list):
+        installation_paths.extend(paths)
+
+# Write to file
 with open("installation_paths.txt", "w") as file:
     for path in installation_paths:
         file.write(path + "\n")
+
+print("Installation paths written to 'installation_paths.txt'")
